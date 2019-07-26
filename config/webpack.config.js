9a5cb7d5194944ee4d0b8add172473619e23e25a
Javey <jiawei23716@sina.com>
2019-07-26 13:36:18 +0800

    revert happypack because it can not improve stylus hot-reload performance

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,6 @@
 const Path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const HappyPack = require('happypack');
 
 const resolve = (path) => Path.resolve(__dirname, path);
 
@@ -31,85 +30,80 @@ module.exports = {
                     resolve('../src'), 
                     resolve('../node_modules/kpc-demo'),
                 ],
-                use: 'happypack/loader?id=js',
-                // use: [
-                    // {
-                        // loader: 'babel-loader',
-                        // options: {
-                            // cacheDirectory: true, 
-                        // }
-                    // }
-                // ]
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true, 
+                        }
+                    }
+                ]
             },
             {
                 test: /\.vdt$/,
-                use: 'happypack/loader?id=vdt',
-                // use: [
-                    // {
-                        // loader: 'babel-loader',
-                        // options: {
-                            // cacheDirectory: true, 
-                        // }
-                    // },
-                    // {
-                        // loader: 'vdt-loader',
-                        // options: {
-                            // delimiters: ['{{', '}}'],
-                            // skipWhitespace: true,
-                        // }
-                    // }
-                // ]
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true, 
+                        }
+                    },
+                    {
+                        loader: 'vdt-loader',
+                        options: {
+                            delimiters: ['{{', '}}'],
+                            skipWhitespace: true,
+                        }
+                    }
+                ]
             },
             {
                 test: /\.styl$/,
-                use: 'happypack/loader?id=styl',
-                // use: [
-                    // {
-                        // loader: 'style-loader',
-                    // },
-                    // {
-                        // loader: 'css-loader', 
-                        // options: {
-                            // url: true,
-                        // }
-                    // },
-                    // {
-                        // loader: 'stylus-loader', 
-                        // options: {
-                            // 'include css': true,
-                            // 'resolve url': true,
-                            // sourceMap: false,
-                            // 'import': resolve('../src/theme/index.styl'),
-                        // }
-                    // }
-                // ]
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader', 
+                        options: {
+                            url: true,
+                        }
+                    },
+                    {
+                        loader: 'stylus-loader', 
+                        options: {
+                            'include css': true,
+                            'resolve url': true,
+                            sourceMap: false,
+                            'import': resolve('../src/theme/index.styl'),
+                        }
+                    }
+                ]
             },
             {
                 test: /\.css$/,
-                use: 'happypack/loader?id=css',
-                // use: [
-                    // {
-                        // loader: 'style-loader',
-                    // },
-                    // {
-                        // loader: 'css-loader',
-                        // options: {
-                            // url: true
-                        // }
-                    // }
-                // ]
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: true
+                        }
+                    }
+                ]
             },
             {
                 test: /\.(woff2?|eot|ttf|otf|svg|png)(\?.*)?$/,
-                use: 'happypack/loader?id=file',
-                // use: [
-                    // {
-                        // loader: 'file-loader',
-                        // options: {
-                            // outputPath: 'static/media/',
-                        // }
-                    // }
-                // ]
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'static/media/',
+                        }
+                    }
+                ]
             },
         ]
     },
@@ -117,83 +111,6 @@ module.exports = {
         new HtmlWebpackPlugin({
             inject: true,
             template: resolve('../public/index.html'),
-        }),
-        new HappyPack({
-            id: 'js',
-            loaders: [
-                {
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true, 
-                    }
-                }
-            ]
-        }),
-        new HappyPack({
-            id: 'vdt',
-            loaders: [
-                {
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true, 
-                    }
-                },
-                {
-                    loader: 'vdt-loader',
-                    options: {
-                        delimiters: ['{{', '}}'],
-                        skipWhitespace: true,
-                    }
-                }
-            ]
-        }),
-        new HappyPack({
-            id: 'styl',
-            loaders: [
-                {
-                    loader: 'style-loader',
-                },
-                {
-                    loader: 'css-loader', 
-                    options: {
-                        url: true,
-                    }
-                },
-                {
-                    loader: 'stylus-loader', 
-                    options: {
-                        'include css': true,
-                        'resolve url': true,
-                        sourceMap: false,
-                        'import': resolve('../src/theme/index.styl'),
-                    }
-                }
-            ]
-        }),
-        new HappyPack({
-            id: 'css',
-            loaders: [
-                {
-                    loader: 'style-loader',
-                },
-                {
-                    loader: 'css-loader',
-                    options: {
-                        url: true
-                    }
-                }
-            ]
-        }),
-        new HappyPack({
-            id: 'file',
-            loaders: [
-                {
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: 'static/media/',
-                    }
-                }
-            ]
         }),
     ],
     devServer: {
