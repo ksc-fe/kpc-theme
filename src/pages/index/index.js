0eb134297a1cf9c8c8bedc300aae9fa02bb394db
Javey <jiawei23716@sina.com>
2019-08-14 11:51:48 +0800

    minimum code preview panel

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -19,6 +19,7 @@ export default class Index extends Intact {
         return {
             components,
             lastSize: '500px',
+            previewSize: '0px',
             variables: [{}],
             code: '',
             globalVariables: [{}],
